bc715de13acd3ac28f694c78d457e1ebbc4c6c1f
sharonhe <hesharon0@gmail.com>
2020-12-28 12:44:23 -0800

    removed comments

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,10 +4,6 @@ import userReducer from "./reducers/userReducer.js";
 import dataReducer from "./reducers/dataReducer.js";
 import uiReducer from "./reducers/uiReducer.js";
 
-// const initialState = {};
-
-// const middleware = [thunk];
-
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const reducers = combineReducers({
@@ -15,15 +11,6 @@ const reducers = combineReducers({
   data: dataReducer,
   UI: uiReducer,
 });
-
-// const store = createStore(
-//   reducers,
-//   initialState,
-//   compose(
-//     applyMiddleware(...middleware),
-//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-//   )
-// );
 
 const store = createStore(
   reducers,
